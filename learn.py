909b7de8b5c9f5631c041da16c2091b98f9e92d0
Nemanja Milicevic <the.nemanja.milicevic@gmail.com>
2019-04-07 17:57:39 +0200

    Save model periodically

--- learn.py
+++ learn.py
@@ -11,11 +11,12 @@ from ppo import PPOAgent
 
 def learn(num_envs: int,
           device: torch.device,  # CUDA or CPU
-          total_steps: int = 2048 * 8 * 512,
+          total_steps: int = 2048 * 8 * 640,
           steps_per_update: int = 2048,
           recurrent_hidden_state_size: int = 384,
           discount=0.99,
-          gae_lambda=0.95):
+          gae_lambda=0.95,
+          save_interval=64):
     envs = MultiprocessEnvironment(num_envs=num_envs)
     actor_critic = RecurrentPolicy(state_frame_channels=envs.observation_shape[0],
                                    action_space_size=envs.action_space_size,
@@ -33,7 +34,7 @@ def learn(num_envs: int,
 
     num_updates = total_steps // (num_envs * steps_per_update)
 
-    for update in tqdm(range(num_updates)):
+    for update_step in tqdm(range(num_updates)):
         for step in range(steps_per_update):
             with torch.no_grad():
                 actor_input = experience_storage.get_actor_input(step)
@@ -60,7 +61,14 @@ def learn(num_envs: int,
                                                gae_lambda=gae_lambda)
 
         losses = agent.update(experience_storage)
-        print(experience_storage.rewards.mean())
+
+        print(experience_storage.rewards.mean(),
+              experience_storage.rewards.std())
+
+        save_model = (update_step % save_interval) == (save_interval - 1)
+        if save_model:
+            model_path = 'models/model_{}.bin'.format(update_step + 1)
+            torch.save(actor_critic.state_dict(), model_path)
 
 
 if __name__ == '__main__':
